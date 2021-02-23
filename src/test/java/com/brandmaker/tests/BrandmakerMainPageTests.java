4d4783887eb97e972bf0e9eaf304be15d461cff4
andreddd799 <36064944+andreddd799@users.noreply.github.com>
2021-02-23 17:43:31 +0300

    added console logs (SEVERE) test

--- src/test/java/com/brandmaker/tests/BrandmakerMainPageTests.java
+++ src/test/java/com/brandmaker/tests/BrandmakerMainPageTests.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.brandmaker.helpers.DriverHelper.getConsoleLogs;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 @Tag("web")
 @Feature("Main Page")
@@ -35,7 +39,6 @@ public class BrandmakerMainPageTests extends TestBase{
     @Test
     @AllureId("1623")
     @DisplayName("Console log should not contain any errors")
-    @Disabled
     public void mainPageConsoleLogTest() {
         step("Open main page", ()-> open(""));
 
@@ -43,8 +46,9 @@ public class BrandmakerMainPageTests extends TestBase{
             $("#hs-en-cookie-confirmation-buttons-area").find(byText("Accept")).click();
         });
 
-//        String consoleLogs = getConsoleLogs();
-//        assertThat(consoleLogs, not(containsString("SEVERE")));
+        step("Main page should not have errors (SEVERE) in console", () -> {
+        String consoleLogs = getConsoleLogs();
+        assertThat(consoleLogs, not(containsString("SEVERE")));
+        });
     }
-
 }
